6499981aa3fdf86ad24407bdde7bb4282148cfa0
pcego <pcego36@gmail.com>
2018-08-09 21:06:46 -0300

    first commit

---
+++ app/src/main/java/android/fasa/edu/br/sanelibras/SplashActivity.java
@@ -0,0 +1,39 @@
+package android.fasa.edu.br.sanelibras;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Splash Actvity Sanelibras
+public class SplashActivity extends AppCompatActivity {
+
+    // Objeto Handler
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        /*
+        Instanciando o objeto handler e utilizando
+        o método postDelayde para atrasar
+        a chamada da MainActivity em 2 segundos
+        */
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent =
+                        new Intent(getApplicationContext(),
+                                MainActivity.class);
+
+                startActivity(intent);
+
+            }
+        }, 2000);
+
+    }
+}
